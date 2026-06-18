0662b773a6b3562af58463a04313cc2fa17e7149
linshengkai-coder <linshengkai.gmis@gmail.com>
2026-06-18 11:48:44 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -56,3 +56,79 @@ DEPT_INFO = {
         'steps': ['Visit Level 2, Wing D', 'Estimated wait: 15–20 min', 'Bring photos of affected area if possible'],
     },
 }
+
+
+def predict_department(inputs: dict) -> tuple[str,float,list]
+ patient_df = pd.DataFrame([inputs])
+ patient_df[cols_to_scale scaler.transform(patient_df[cols_to_scale])
+ #run the model
+ predicted_class = model.predict(patient_df[features])[0]
+ all_proba = model.predict_proba(patient_df[features])[0]
+
+# Convert Number to human readable label
+ dept_name = dept_map_inv[predicted_class]
+ confidence = all_proba[predicted_class]*100
+
+ return dept_name, confidence, all_proba
+
+def show_header():
+ st.markdown("""
+  <div style="background: linear-gradient(135deg, #1e3a8a,#1a56db,#0ea5e9);
+              padding:3rem 2rem; text-align: center; margin-bottom: 2rem:">
+      <p style = "color: #ededed ; font-size: 13px;
+          FUTURE CLASSROOM: Machine Learning
+      </p>
+      <h1 style = "color:white;">
+        Smart Hospital Patient Navigator
+      </h1>
+      <p style = color : white;font-size: 18px;>
+          Find the right department for your symptoms
+      </p>
+  </div>
+ """, unsafe_allow_html=True)
+
+def show_symptom_section():
+    """Returns a dict of symptom checkboxes {symptom_name: True/False}"""
+    st.subheader("1. What are your main symptoms?")
+
+    col1, col2, col3, col4 = st.columns(4)
+    symptoms = {}
+
+    with col1:
+        symptoms['fever'] = st.checkbox("🌡️ Fever")
+        symptoms['cough'] = st.checkbox("🤧 Cough")
+    with col2:
+        symptoms['headache']   = st.checkbox("🤕 Headache")
+        symptoms['chest_pain'] = st.checkbox("💔 Chest Pain")
+    with col3:
+        symptoms['stomach_pain']     = st.checkbox("🤢 Stomach Pain")
+        symptoms['shortness_breath'] = st.checkbox("😮‍💨 Shortness of Breath")
+    with col4:
+        symptoms['nausea_vomiting'] = st.checkbox("🤮 Nausea / Vomiting")
+        symptoms['dizziness']       = st.checkbox("😵 Dizziness")
+
+    col5, _, _, _ = st.columns(4)
+    with col5:
+        symptoms['skin_rash'] = st.checkbox("🔴 Skin Rash")
+
+    return symptoms
+
+def main():
+    st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon='🏥', layout="wide")
+
+    show_header()
+
+
+if__name__=="__main__":
+   main()
+
+
+
+
+      
+
+
+
+
+
+
